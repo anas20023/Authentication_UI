f9b9ff2a7176ca26b927fe18bad267fd74295cf3
Anas Ibn Belal <yorder07@gmail.com>
2024-10-03 18:51:02 +0600

    Refactor App component to add authentication functionality with Clerk

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,33 @@
-import Navbar from "./Components/navigationbar";
-import FormContainer from "./Components/FormContainer";
-function App() {
-  return (
-    <>
-      <Navbar />
-      <FormContainer />
-    </>
-  )
-}
+import { SignedIn, SignedOut, SignIn, SignUp, UserButton } from "@clerk/clerk-react";
+import Navbar from './Components/navigationbar';
 
-export default App;
+export default function App() {
+  return (
+    <div className="flex flex-col min-h-screen">
+      <header>
+        <Navbar />
+        <SignedOut>
+          <SignIn path="/sign-in" routing="path" signUpUrl="/sign-up" />
+        </SignedOut>
+        <SignedIn>
+          <UserButton />
+        </SignedIn>
+      </header>
+
+      <main className="flex-grow flex items-center justify-center">
+        <SignedOut>
+          {/* Check if this component is loading */}
+          <SignUp path="/sign-up" routing="path" signInUrl="/sign-in" />
+        </SignedOut>
+
+        <SignedIn>
+          <h1 className="text-2xl">Welcome to the Dashboard!</h1>
+        </SignedIn>
+      </main>
+
+      <footer className="bg-gray-800 p-4 text-center text-white">
+        <p>&copy; {new Date().getFullYear()} Your Company. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
